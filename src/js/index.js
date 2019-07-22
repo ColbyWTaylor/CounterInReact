1e8dc09fde28bdfe7b1903271877d9135ac0061b
ColbyWTaylor <colbyt.093@gmail.com>
2019-07-22 05:04:22 +0000

    working :)

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
 
 //include bootstrap npm library into the bundle
 import "bootstrap";
@@ -14,14 +15,46 @@ function SimpleCounter(props) {
 			<div className="col digit col">
 				<i className="far fa-clock" />
 			</div>
-			<div className="five digit col">0</div>
-			<div className="four digit col">0</div>
-			<div className="three digit col">0</div>
-			<div className="two digit col">0</div>
-			<div className="one digit col">2</div>
+			<div className="four digit col">
+				<p>{props.digitFour}</p>
+			</div>
+			<div className="three digit col">
+				<p>{props.digitThree}</p>
+			</div>
+			<div className="two digit col">
+				<p>{props.digitTwo}</p>
+			</div>
+			<div className="one digit col">
+				<p>{props.digitOne}</p>
+			</div>
 		</div>
 	);
 }
 
-//render your react application
-ReactDOM.render(<SimpleCounter />, document.querySelector("#app"));
+SimpleCounter.propTypes = {
+	digitFour: PropTypes.number,
+	digitThree: PropTypes.number,
+	digitTwo: PropTypes.number,
+	digitOne: PropTypes.number
+};
+
+let counter = 0;
+
+setInterval(function() {
+	const four = Math.floor(counter / 10000);
+	const three = Math.floor(counter / 1000);
+	const two = Math.floor(counter / 10);
+	const one = Math.floor(counter / 1);
+	// console.log(four);
+	counter++;
+	//render your react application
+	ReactDOM.render(
+		<SimpleCounter
+			digitOne={one}
+			digitTwo={two}
+			digitThree={three}
+			digitFour={four}
+		/>,
+		document.querySelector("#app")
+	);
+}, 1000);
