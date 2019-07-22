0a7b68d1c0e45036fab7137954e4698c3cf359e2
ColbyWTaylor <colbyt.093@gmail.com>
2019-07-22 04:02:02 +0000

    first

--- src/js/index.js
+++ src/js/index.js
@@ -8,8 +8,20 @@ import "bootstrap";
 //include your index.scss file into the bundle
 import "../styles/index.scss";
 
-//import your own components
-import { Home } from "./component/home.js";
+function SimpleCounter(props) {
+	return (
+		<div className="bigCounter row" style={{ backgroundColor: "black" }}>
+			<div className="col digit col">
+				<i className="far fa-clock" />
+			</div>
+			<div className="five digit col">0</div>
+			<div className="four digit col">0</div>
+			<div className="three digit col">0</div>
+			<div className="two digit col">0</div>
+			<div className="one digit col">2</div>
+		</div>
+	);
+}
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<SimpleCounter />, document.querySelector("#app"));
